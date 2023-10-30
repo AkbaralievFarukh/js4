37cbd1d513e0a666446df97ba0b66a44fbf639d4
Farukh <akbfarukh@gmail.com>
2023-10-30 20:58:20 +0600

    modifided

--- script.js
+++ script.js
@@ -29,4 +29,32 @@
 
 
 
+// Задание 1
+
+// function checkDomain(domain) {
+//     if (domain.endsWith('.com') || domain.endsWith('.ru')){
+//         return `${domain} have . and ru or com`
+//     } else {
+//         return `${domain} havent . and ru or com`
+//     }
+// }
+//
+// console.log(checkDomain('google.com'))
+// console.log(checkDomain('yandex.ru'))
+// console.log(checkDomain('softech.kg'))
+
+// Задание 2
+
+// function checkWordLength(word) {
+//     if (word.length % 2 === 0 && word.length >= 4 && word.length <= 8) {
+//         return `Длина слова ${word} удовлетворяет условиям.`; // Длина слова удовлетворяет условиям
+//     } else {
+//         return `Длина слова ${word} не удовлетворяет условиям.`; // Длина слова не удовлетворяет условиям
+//     }
+// }
+//
+// console.log(checkWordLength('Farukh'))
+// console.log(checkWordLength('Alisovamaksimova'))
+
+// Задание 3
 
