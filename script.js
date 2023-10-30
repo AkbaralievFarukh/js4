e044a7fa1eed08ca78c9bb6161e97b79672f9543
Farukh <akbfarukh@gmail.com>
2023-10-30 12:21:03 +0600

    modifided

--- script.js
+++ script.js
@@ -10,7 +10,19 @@
 // let arr = [null, undefined, 2, 'hello', true]
 // console.log(arr.map(el => typeof el ))
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log(arr.map(el => {
-    return el === 5 ? el * el : el
-}))
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(arr.map(el => {
+//     return el === 5 ? el * el : el
+// }))
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(arr.filter(el => el % 2 === 0))
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(arr.map((el, idx) => idx % 2 === 0))
+
+// let arr = ['Aidin', 'Farukh', 'Shabdan', 'Kamilla']
+// console.log(arr.filter(item => item.length % 2 === 0))
+
+let arr = ['Aidin', 'Farukh', 'Shabdan', 'Kamilla', 'Karina']
+console.log(arr.filter(el => el.includes('K')))
