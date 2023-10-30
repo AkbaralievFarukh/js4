f30c392c278e1deb33b73bdace44ddcf085504d3
Farukh <akbfarukh@gmail.com>
2023-10-30 22:04:21 +0600

    modifided

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// let str = 'farukh'
-// console.log(str.split('').reverse().join(''));
-//
-// let students = ['Farukh', 'Shabdan', 'Alina']
-// console.log(students.map(student => student.toUpperCase()));
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// console.log(arr.map(el => el * 4))
-
-// let arr = [null, undefined, 2, 'hello', true]
-// console.log(arr.map(el => typeof el ))
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// console.log(arr.map(el => {
-//     return el === 5 ? el * el : el
-// }))
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// console.log(arr.filter(el => el % 2 === 0))
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// console.log(arr.map((el, idx) => idx % 2 === 0))
-
-// let arr = ['Aidin', 'Farukh', 'Shabdan', 'Kamilla']
-// console.log(arr.filter(item => item.length % 2 === 0))
-
-// let arr = ['Aidin', 'Farukh', 'Shabdan', 'Kamilla', 'Karina']
-// console.log(arr.filter(el => el.includes('K')))
-
-
-
 // Задание 1
 
 // function checkDomain(domain) {
@@ -58,3 +27,24 @@
 
 // Задание 3
 
+// function replaceWord(word) {
+//     return [...word].map((char, index) => (index % 2 === 0) ? 'E' : char).join('');
+// }
+//
+// console.log(replaceWord('Farukh'))
+
+// Задание 4
+
+// function passwordChecker (pass1, pass2) {
+//     pass1 = pass1.trim()
+//     pass2 = pass2.trim()
+//
+//     if (pass1 === pass2 && pass1 !== '' && pass2 !== '') {
+//         return `Пароли совпадают и не содержат пробелы в начале`
+//     } else {
+//         return `Пароли не совпадают или пусты`
+//     }
+// }
+//
+// console.log(passwordChecker('170707', '170707'))
+// console.log(passwordChecker('130303', '170707'))
